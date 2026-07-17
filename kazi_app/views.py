0316f27180ebd71e0763544fb4a2508b0d90e9c0
feralsyntax <fullstack.benie@gmail.com>
2026-07-17 11:26:15 +0300

    add (views): create a method to handle token generation

--- kazi_app/views.py
+++ kazi_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
+
+def generate_token(user):
+    token = RefreshToken.for_user(user)
+
+    return {
+        "message": "Request successful",
+        "access_token": str(token.access_token),
+        "refresh_token": str(token),
+    }
